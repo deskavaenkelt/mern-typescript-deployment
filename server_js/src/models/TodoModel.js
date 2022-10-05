91784a87db6cbd8b4aa8432412400397824ac2bf
Lars Strömberg <lars@aid.se>
2022-10-05 14:06:25 +0200

    fix

--- server_js/src/models/TodoModel.js
+++ server_js/src/models/TodoModel.js
@@ -3,18 +3,9 @@ import mongoose from 'mongoose'
 const dbCollection = process.env.MONGODB_COLLECTION
 
 const todoSchema = new mongoose.Schema({
-    title: {
-        type: 'string',
-        required: true
-    },
-    completed: {
-        type: 'boolean',
-        required: true
-    },
-    assignedTo: {
-        type: 'string',
-        required: true
-    }
+    title: 'string',
+    completed: 'boolean',
+    assignedTo: 'string'
 }, {
     timestamps: true
 })
